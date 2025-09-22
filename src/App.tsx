3852b288ed5dd0f6a0aab8063a335498806a58d6
Jay Gadher <jaygadher78@gmail.com>
2025-09-22 11:06:28 +0530

    scrolig page

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import AboutUs from "./pages/AboutUs";
 import ContactUs from "./pages/ContactUs";
 import Profile from "./pages/Profile";
 import ProductDetail from "./pages/ProductDetail";
+import ScrollToTopOnRoute from "@/components/ScrollToTopOnRoute";
 
 const queryClient = new QueryClient();
 
@@ -24,6 +25,7 @@ const App = () => (
         <Toaster />
         <Sonner />
         <BrowserRouter>
+          <ScrollToTopOnRoute />
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/shop" element={<Shop />} />
