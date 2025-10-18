9b18811934156c4ab8ed7beeacbef5c7706ef1bb
jaygadher <jaygadher78@gmail.com>
2025-10-18 19:16:21 +0530

    Update Sunday business hours in ContactUs page

--- src/pages/ContactUs.tsx
+++ src/pages/ContactUs.tsx
@@ -177,7 +177,7 @@ const ContactUs = () => {
                         <p className="text-muted-foreground">
                           Monday - Friday: 9:00 AM - 6:00 PM<br />
                           Saturday: 10:00 AM - 4:00 PM<br />
-                          Sunday: Closed
+                          Sunday: 10:00 AM - 2:00 PM
                         </p>
                       </div>
                     </div>
