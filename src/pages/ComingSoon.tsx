b5e72f06e35b203aa152ba3068921da5efc511e7
Dev0081118 <divyarajsinhrana0081@gmail.com>
2025-12-25 22:36:12 +0530

    Comment out  with space the ComingSoon component code

--- src/pages/ComingSoon.tsx
+++ src/pages/ComingSoon.tsx
@@ -1,4 +1,4 @@
-/*import { useState, useEffect } from "react";
+/* import { useState, useEffect } from "react";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import AuthModal from "@/components/AuthModal";
@@ -120,3 +120,4 @@ const ComingSoon = () => {
 export default ComingSoon;
 
 */
+
