24ae5713c00fe77e466546f864df8d9a311144d4
Dev0081118 <divyarajsinhrana0081@gmail.com>
2025-12-25 22:51:42 +0530

    Fix Index route coming soon component name in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -36,7 +36,7 @@ const App = () => {
           <BrowserRouter>
             <ScrollToTopOnRoute />
             <Routes>
-              <Route path="/" element={<ComingSoon />} />
+              <Route path="/" element={<index/>} />
               <Route path="/shop" element={<ShopifyShop />} />
               <Route path="/shopify-shop" element={<ShopifyShop />} />
               <Route path="/product/:id" element={<ProductDetail />} />
@@ -56,5 +56,6 @@ const App = () => {
 };
 
 export default App;
+
 
 
